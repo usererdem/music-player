6134b84f05a4722cf57b694320243a44ccdf6b4b
usererdem <erdemelvan35@gmail.comm>
2023-04-23 17:06:47 +0300

    uplifting props to be able to use audioRef on library onClick to library song play song functionality added

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,12 +1,22 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-const Library = ({songs}) => {
+const Library = ({ songs, setCurrentSong, audioRef, isPlaying }) => {
   return (
     <div className='library'>
       <h2>Library</h2>
       <div className='library-songs'>
-        {songs.map(song => <LibrarySong song={song} /> )}
+        {songs.map((song) => (
+          <LibrarySong
+            song={song}
+            songs={songs}
+            setCurrentSong={setCurrentSong}
+            id={song.id}
+            key={song.id}
+            audioRef={audioRef}
+            isPlaying={isPlaying}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,10 +1,31 @@
 import React from "react";
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({
+  song,
+  songs,
+  setCurrentSong,
+  id,
+  audioRef,
+  isPlaying,
+}) => {
+  const songSelectHandler = () => {
+    const selectedSong = songs.filter((state) => state.id === song.id);
+    setCurrentSong(selectedSong[0]);
+    /* setCurrentSong(song); */
+    // check if the song is playing
+    if (isPlaying) {
+      const playPromise = audioRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.then((audio) => {
+          audioRef.current.play();
+        });
+      }
+    }
+  };
   return (
-    <div className='library-song'>
+    <div onClick={songSelectHandler} className='library-song'>
       <img src={song.cover} alt={song.name}></img>
-      <div className="song-description">
+      <div className='song-description'>
         <h3>{song.name}</h3>
         <h4>{song.artist}</h4>
       </div>
