a7d6504b05a998dd5ed53f484671eeafcf215174
usererdem <erdemelvan35@gmail.comm>
2023-04-23 04:28:56 +0300

    Folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+//Adding Components
+import Player from "./components/Player";
+import Song from "./components/Song";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
